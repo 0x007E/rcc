c472e13a9fc30ebed5ec981c40ba3085e8ddd325
0x007E <raffael@sunriax.at>
2025-09-29 09:03:16 +0200

    Fix missing settings

--- firmware/RCC_FW_1_0/main.c
+++ firmware/RCC_FW_1_0/main.c
@@ -4,7 +4,7 @@
 #define SWITCH_COMMAND_EXECUTE_MS 3000UL
 #define SWITCH_SYSTEM_OFF_TIME_MS 3000UL
 #define COLOR_FADE_DELAY_MS 10UL
-//#define ENABLE_EEPROM_WRITE
+#define ENABLE_EEPROM_WRITE
 
 #include <avr/io.h>
 #include <avr/sleep.h>
@@ -24,14 +24,14 @@ char EEMEM copyright[] = "Copyright 2025 g.raf engineering";
 char EEMEM github[] = "github.com/0x007e/rcc";
 
 LED_Data EEMEM ee_led1  = {
-	0x01,
+	0x03,
 	0x0F,
 	0x00,
 	0x0A
 };
 
 LED_Data EEMEM ee_led2  = {
-	0x01,
+	0x03,
 	0x00,
 	0x0F,
 	0x0A
@@ -61,13 +61,14 @@ volatile unsigned long systick;
 ISR(TCA0_OVF_vect)
 {
     systick++;
+	TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
 }
 
 static void timer_init(void)
 {	
-	TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
-	TCA0.SINGLE.PER = 0x0270;
-	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV8_gc | TCA_SINGLE_ENABLE_bm;
+	TCA0.SINGLE.INTCTRL |= TCA_SINGLE_OVF_bm;
+	TCA0.SINGLE.PER = 0x04E1;
+	TCA0.SINGLE.CTRLA |= TCA_SINGLE_CLKSEL_DIV8_gc | TCA_SINGLE_ENABLE_bm;
 }
 
 static void timer_disable(void)
@@ -135,21 +136,21 @@ int main(void)
 
 		if(PORTA.IN & SWITCH)
 		{
-    		led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Ready, LED_MIN_INTENSITY), LED_Delay_MS_100, 0);
+			led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Ready, LED_MIN_INTENSITY), LED_Delay_MS_100, 0);
 			
 			switch_count++;
 			last_button_press = systick;
 			
 			while(PORTA.IN & SWITCH)
-            {
-                if((systick - last_button_press) > SWITCH_SYSTEM_OFF_TIME_MS)
-                {
-                    led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Ready,   LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
-                    led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Warning, LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
-                    led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Error,   LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
-                    system_shutdown();
-                }
-            }
+			{
+				if((systick - last_button_press) > SWITCH_SYSTEM_OFF_TIME_MS)
+				{
+					led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Ready,   LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
+					led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Warning, LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
+					led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Error,   LED_MIN_INTENSITY), LED_Delay_MS_500, 0);
+					system_shutdown();
+				}
+			}
 		}
 
         if((switch_count > 0) && ((systick - last_button_press) > SWITCH_COMMAND_EXECUTE_MS))
@@ -172,7 +173,7 @@ int main(void)
 				else
 				{
     				led = &led2;
-					led_blink(LED_Position_Left, led_status_color(LED_Status_Ready, LED_MIN_INTENSITY), LED_Delay_MS_500, 1);
+					led_blink(LED_Position_Right, led_status_color(LED_Status_Ready, LED_MIN_INTENSITY), LED_Delay_MS_500, 1);
 				}
 				
 				if(PORTA.IN & SWITCH)
@@ -188,14 +189,11 @@ int main(void)
                         position = LED_Position_Left;
                     }
                     last_button_press = systick;
-
-					while(PORTA.IN & SWITCH);
 				}
-				_delay_ms(10);
 
 			} while ((systick - last_button_press) < SWITCH_COMMAND_EXECUTE_MS);
 			
-			switch (switch_count)
+			switch (execute_command)
 			{
 				case 1:
 					while(!(PORTA.IN & SWITCH))
@@ -251,7 +249,10 @@ int main(void)
                 }
             #endif
 
+			led_blink(LED_Position_Left | LED_Position_Right_Alternating, led_status_color(LED_Status_Ready, LED_MIN_INTENSITY), LED_Delay_MS_500, 2);
+
 			execute_command = 0;
+			switch_count = 0;
 		}
     }
 }
